f8c4295440ed505bc1463ce8fcd4fd24536d70d4
gangwar22 <147224060+gangwar22@users.noreply.github.com>
2024-07-03 21:42:08 +0530

    add folder

---
+++ Frontend/src/Login/Login.jsx
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+import './Login.css';
+import axios from 'axios';
+import { useNavigate, Link } from 'react-router-dom';
+
+function Login() {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        'https://backend-code-online-learning.onrender.com/Login',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          withCredentials: true
+        }
+      );
+
+      const data = response.data;
+      console.log(data);
+      if (data.message !== "user login successfully..") {
+        alert('Invalid credentials');
+      } else {
+        alert('Login successful!');
+        localStorage.setItem('authToken', data.token);
+        navigate('/');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert('Error logging in. Please try again.');
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="login-box">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="input-box">
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+            <label>Email</label>
+          </div>
+          <div className="input-box">
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+            <label>Password</label>
+          </div>
+          <div className="forgot-pass">
+            <a href="#">Forgot your password?</a>
+          </div>
+          <button type="submit" className="btn">
+            Login
+          </button>
+          <div className="signup-link">
+            <p>
+              Have no account yet? <Link to="/signup">Signup</Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ Frontend/src/assignment_section/layout.jsx
@@ -0,0 +1,42 @@
+import { BrowserRouter as  Route, Routes } from 'react-router-dom';
+import Sidebar from './sidebar';
+import TopNav from './topNav';
+import AssignmentList from './assignment_list';
+import AssignmentDetails from './assignment_details';
+import SubmissionSuccess from './submission';
+import usePageTitle from './usepage_title';
+
+function Layout() {
+  const title = usePageTitle();
+
+  return (
+    <div className="layout" id='layout'>
+      <Sidebar />
+      <div className="mains">
+        <TopNav title={title} />
+        <div className="content">
+          <Routes>
+            <Route path="/assignments/all" element={<AssignmentList />} />
+            <Route path="/assignments/running" element={<AssignmentList status="Running" />} />
+            <Route path="/assignments/completed" element={<AssignmentList status="Completed" />} />
+            <Route path="/assignment/:id" element={<AssignmentDetailsWrapper />} />
+            <Route path="/profile" element={<h1>Profile</h1>} />
+            <Route path="/submission-success" element={<SubmissionSuccess />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function AssignmentDetailsWrapper() {
+  return (
+    <div className="details-wrapper">
+      <div className="assignment-details-section">
+        <AssignmentDetails />
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
